8a6815cbabbe35e09df7752eb7a9b8655b6ccccf
danielperezec <daperezj@utn.edu.ec>
2024-02-04 13:17:53 -0500

    Inicio Esquema Grupal

---
+++ src/scripts.js
@@ -0,0 +1,8 @@
+// JavaScript para manejar la lógica del reproductor
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Lógica de inicio de sesión
+    // Lógica de reproducción de música
+    // Lógica de gestión de la playlist
+});
+
